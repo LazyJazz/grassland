7885c07b028b71f95675563a684c8817a2560511
LazyJazz <zijian.lv@hotmail.com>
2023-02-09 19:49:52 +0800

    Can we do this?

--- .github/test/windows-cmake.bat
+++ .github/test/windows-cmake.bat
@@ -1,3 +1,4 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G "Ninja" -B %1 -DCMAKE_BUILD_TYPE=%2 -DCMAKE_C_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.34.31933/bin/Hostx64/x64/cl.exe"  -DCMAKE_CXX_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.34.31933/bin/Hostx64/x64/cl.exe"
+cmake --build %1 -j
