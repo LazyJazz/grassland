80ffce94f7d33ce3c8d1726e3e1c66ce56de54aa
LazyJazz <zijian.lv@hotmail.com>
2023-04-05 21:41:49 +0800

    Restore rho_air

--- demo/fluid_cuda/params.h
+++ demo/fluid_cuda/params.h
@@ -16,7 +16,7 @@
 #define SIZE_Z (GRID_SIZE_Z * DELTA_X)
 #define TYPE_AIR 0
 #define TYPE_LIQ 1
-#define RHO_AIR 0.005f
+#define RHO_AIR 0.01f
 #define RHO_LIQ 1.0f
 #define GRID_POINT_ID(x, y, z) \
   ((x)*GRID_SIZE_Y * GRID_SIZE_Z + (y)*GRID_SIZE_Z + (z))
