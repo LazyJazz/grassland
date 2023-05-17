ea365adf5cb97a9014794bb53940cd6436c164f8
LazyJazz <zijian.lv@hotmail.com>
2023-05-17 15:46:04 +0800

    NoGUI mode

--- demo/fluid_large_cuda/main.cu
+++ demo/fluid_large_cuda/main.cu
@@ -1,8 +1,20 @@
 #include "fluid_large.cuh"
+#include <memory>
 
+void GUI(const PhysicSettings& settings) {
+  FluidLarge fluid_large("Fluid Large", 1024, 1024, settings);
+  fluid_large.Run();
+}
+
+[[noreturn]] void NoGUI(const PhysicSettings& settings) {
+  PhysicSolver solver(settings);
+  while (true) {
+    solver.UpdateStep();
+  }
+}
 
 int main() {
   PhysicSettings settings{};
-  FluidLarge fluid_large("Fluid Large", 1024, 1024, settings);
-  fluid_large.Run();
+  // GUI(settings);
+  NoGUI(settings);
 }
