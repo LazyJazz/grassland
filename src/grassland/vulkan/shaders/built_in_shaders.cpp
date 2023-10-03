f14d6a4fb3d2d55bf37521b83362707c0e78c4a1
LazyJazz <zijian.lv@hotmail.com>
2023-10-03 13:43:20 +0800

    Fix xxd issues on Unix-like systems

--- src/grassland/vulkan/shaders/built_in_shaders.cpp
+++ src/grassland/vulkan/shaders/built_in_shaders.cpp
@@ -5,6 +5,7 @@ namespace grassland::vulkan::built_in_shaders {
 namespace {
 #include "built_in_shaders.inl"
 }
+
 std::string GetShader(const std::string &name) {
   if (name == "example.vert") {
     return {reinterpret_cast<const char *>(example_vert), example_vert_len};
