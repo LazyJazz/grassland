b24b68bc0a9656419caa764d81006489bdcbd287
LazyJazz <zijian.lv@hotmail.com>
2023-10-14 15:30:00 +0800

    Vulkan ray tracing revive

--- src/grassland/vulkan/core/device.cpp
+++ src/grassland/vulkan/core/device.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "grassland/vulkan/core/device.h"
 
 #include <map>
--- src/grassland/vulkan/raytracing/shader_binding_table.h
+++ src/grassland/vulkan/raytracing/shader_binding_table.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "grassland/vulkan/raytracing/ray_tracing_pipeline.h"
 
 namespace grassland::vulkan::raytracing {}
