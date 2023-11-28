2a90c5050b321288f6838f4368b03f85b9035970
LazyJazz <zijian.lv@hotmail.com>
2023-11-28 15:29:29 +0800

    Update workflow

--- .github/test/windows-cmake-clang.bat
+++ .github/test/windows-cmake-clang.bat
@@ -1,4 +1,4 @@
 @echo off
 cd %1
-cmake -G "Ninja" -B build-clang -DCMAKE_BUILD_TYPE=%2 "-DCMAKE_C_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang.exe" "-DCMAKE_CXX_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang++.exe" "-DCMAKE_RC_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/llvm-rc.exe"  -DCMAKE_TOOLCHAIN_FILE=external/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build-clang -j
+cmake -G "Ninja" -B build-clang-%2 -DCMAKE_BUILD_TYPE=%2 "-DCMAKE_C_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang.exe" "-DCMAKE_CXX_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang++.exe" "-DCMAKE_RC_COMPILER=C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/llvm-rc.exe"
+cmake --build build-clang-%2 -j
