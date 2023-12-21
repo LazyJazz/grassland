e86345dbd91dbb8397d6a01cb5783c6f76b03a02
Zijian Lyu <zijian.lv@hotmail.com>
2023-12-21 11:17:29 +0800

    File name extension to shader stage (#92)

--- src/grassland/vulkan/shaders/built_in_shaders.h
+++ src/grassland/vulkan/shaders/built_in_shaders.h
@@ -2,7 +2,12 @@
 
 #include "grassland/vulkan/resources/shader_module.h"
 
-namespace grassland::vulkan::built_in_shaders {
+namespace grassland::vulkan {
+
+VkShaderStageFlagBits FileNameExtensionToShaderStage(const std::string &name);
+
+namespace built_in_shaders {
 std::vector<std::string> ListAllBuiltInShaders();
 std::vector<uint32_t> GetShaderCompiledSpv(const std::string &name);
-}  // namespace grassland::vulkan::built_in_shaders
+}  // namespace built_in_shaders
+}  // namespace grassland::vulkan
